dd6eceae7038247cd05e3c3ce84e165493212ec8
mirhalay <mirhalay@tutanota.com>
2023-03-27 12:51:19 +0300

    intl for eng, tur

--- src/App/Home/index.js
+++ src/App/Home/index.js
@@ -1,13 +1,25 @@
-import { Card, Container, Form, Row, Spinner, Stack } from "react-bootstrap";
+import {
+  Card,
+  Col,
+  Container,
+  Form,
+  Row,
+  Spinner,
+  Stack,
+} from "react-bootstrap";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import chaptersJSON from "../../chapters.json";
+import { FormattedMessage, useIntl } from "react-intl";
 
 function CheckType({ selectedType, onChange }) {
+  const { $t } = useIntl();
+
   return (
     <Form.Group>
-      <Stack direction="horizontal" className="d-flex align-items-end">
+      <Stack direction="horizontal" className="d-flex align-items-end pt-2">
         <Form.Check
-          label="Meccan"
+          label={$t({ id: "meccan" })}
           checked={selectedType.meccan}
           onChange={onChange}
           value="meccan"
@@ -15,7 +27,7 @@ function CheckType({ selectedType, onChange }) {
         />
 
         <Form.Check
-          label="Medinan"
+          label={$t({ id: "medinan" })}
           checked={selectedType.medinan}
           onChange={onChange}
           value="medinan"
@@ -26,13 +38,8 @@ function CheckType({ selectedType, onChange }) {
 }
 
 function ComboChapters({ selectedType, selectedChapter, setSelectedChapter }) {
-  const [chapters, setChapters] = useState();
-
-  useEffect(() => {
-    axios
-      .get("tr-chapters.json")
-      .then(({ status, data }) => status === 200 && setChapters(data));
-  }, []);
+  const [chapters] = useState(chaptersJSON);
+  const { locale, $t } = useIntl();
 
   const chaptersFilteredByType = chapters?.filter((i) => selectedType[i.type]);
 
@@ -41,11 +48,11 @@ function ComboChapters({ selectedType, selectedChapter, setSelectedChapter }) {
       value={selectedChapter}
       onChange={(i) => setSelectedChapter(i.target.value)}
     >
-      <option value={0}>[Not Selected]</option>
+      <option value={0}>[{$t({ id: "not_selected" })}]</option>
       {chapters &&
         chaptersFilteredByType.map((i) => (
           <option key={i.id} value={i.id}>
-            {i.transliteration} ({i.id})
+            {i[locale.substring(0, 2)]} ({i.id})
           </option>
         ))}
     </Form.Select>
@@ -56,7 +63,26 @@ function ComboChapters({ selectedType, selectedChapter, setSelectedChapter }) {
 //   Math.floor(Math.random() * (max - min + 1) + min);
 
 function useChapterContent() {
+  const { locale } = useIntl();
   const [chapterContent, setChapterContent] = useState(null);
+
+  useEffect(() => {
+    setChapterContent(null);
+  }, [locale]);
+
+  const chapterGetter = (chapter) => {
+    setChapterContent(undefined);
+    axios
+      .get(`chapters/${[locale.substring(0, 2)]}/${chapter}.json`)
+      .then(({ status, data }) => {
+        status === 200 && setChapterContent(data);
+      });
+  };
+
+  useEffect(() => {
+    if (chapterContent) chapterGetter(chapterContent.id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [locale]);
 
   return {
     chapterContent,
@@ -64,42 +90,36 @@ function useChapterContent() {
     setChapterContent,
     getChapter: (selectedChapter) => {
       if (selectedChapter > 0) {
-        setChapterContent(undefined);
-        axios
-          .get(`chapters/en/${selectedChapter}.json`)
-          .then(({ status, data }) => {
-            status === 200 && setChapterContent(data);
-          });
+        chapterGetter(selectedChapter);
       } else setChapterContent(null);
     },
   };
 }
 
 function ShowChapterContent({ chapterContent, isLoading }) {
+  const { $t } = useIntl();
+  const typeLabel = chapterContent && $t({ id: chapterContent.type });
+
   return chapterContent ? (
     <>
-      <h5 className="mt-4 mb-2">
-        {chapterContent.translation} {`[${chapterContent.type}]`}
+      <h5 className="mt-4 mb-3">
+        {chapterContent.translation} {`[${typeLabel}]`}
       </h5>
 
-      <ol>
-        {chapterContent.verses.map((i) => (
-          <Card key={i.id} className="my-2">
-            <li className="py-1 ps-1">
-              <span className="text-dark">{i.translation}</span>
-            </li>
-          </Card>
-        ))}
-      </ol>
+      {chapterContent.verses.map((i) => (
+        <Card key={i.id} className="my-3 ps-2 border-1">
+          <Card.Text className="text-dark">
+            {i.id} - {i.translation}
+          </Card.Text>
+        </Card>
+      ))}
     </>
-  ) : isLoading ? (
-    <Container className="p-2">
-      <Spinner />
-    </Container>
   ) : (
-    <Container className="p-2">
-      <p>please select a chapter to show.</p>
-    </Container>
+    isLoading && (
+      <Container className="p-2">
+        <Spinner />
+      </Container>
+    )
   );
 }
 
@@ -119,40 +139,50 @@ export default function Home() {
   }, [selectedChapter]);
 
   return (
-    <Container className="p-5">
-      <Form className="px-2 border p-3">
-        <Container>
-          <Stack direction="horizontal">
-            <Form.Text className="me-3">Type: </Form.Text>
-            <CheckType
-              selectedType={selectedType}
-              onChange={(i) => {
-                setSelectedChapter(0);
-                setSelectedType({
-                  ...selectedType,
-                  [i.target.value]: i.target.checked,
-                });
-              }}
-            />
-          </Stack>
+    <div className="p-3">
+      <Container className="px-md-4 px-xs-2">
+        <Form className="border pt-3 pb-4 ps-3 ps-md-5">
+          <Row className="align-items-center">
+            <Col md="auto">
+              <Stack direction="horizontal">
+                <Form.Text className="me-3">
+                  <FormattedMessage id="type" />:
+                </Form.Text>
+                <CheckType
+                  selectedType={selectedType}
+                  onChange={(i) => {
+                    setSelectedChapter(0);
+                    setSelectedType({
+                      ...selectedType,
+                      [i.target.value]: i.target.checked,
+                    });
+                  }}
+                />
+              </Stack>
+            </Col>
 
-          <Stack direction="horizontal" className="mt-3">
-            <Form.Text className="me-3">Chapter: </Form.Text>
-            <Row className="ms-1">
-              <ComboChapters
-                selectedType={selectedType}
-                selectedChapter={selectedChapter}
-                setSelectedChapter={setSelectedChapter}
-              />
-            </Row>
-          </Stack>
-        </Container>
-      </Form>
+            <Col md={4} className="ps-md-4">
+              <Stack direction="horizontal" className="mt-3 align-items-start">
+                <Form.Text className="me-3">
+                  <FormattedMessage id="chapter" />:
+                </Form.Text>
+                <Row className="ms-1">
+                  <ComboChapters
+                    selectedType={selectedType}
+                    selectedChapter={selectedChapter}
+                    setSelectedChapter={setSelectedChapter}
+                  />
+                </Row>
+              </Stack>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
 
       <ShowChapterContent
         isLoading={isLoading}
         chapterContent={chapterContent}
       />
-    </Container>
+    </div>
   );
 }
--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FormCheck, Stack } from "react-bootstrap";
 import { IntlProvider } from "react-intl";
 import { LOCALES, messages } from "../locales";
 import Home from "./Home";
@@ -6,9 +7,7 @@ import Home from "./Home";
 const DEFAULT_LOCALE = LOCALES.TURKISH;
 
 export default function App() {
-  const [languageLocale] = useState(
-    localStorage.getItem("locale") ?? LOCALES.TURKISH
-  );
+  const [languageLocale, setLanguageLocale] = useState(LOCALES.TURKISH);
 
   return (
     <IntlProvider
@@ -16,7 +15,33 @@ export default function App() {
       locale={languageLocale}
       defaultLocale={DEFAULT_LOCALE}
     >
-      <Home />
+      <>
+        <div className="border-bottom py-2 px-4">
+          <Stack direction="horizontal">
+            <FormCheck
+              className="me-2"
+              value={LOCALES.TURKISH}
+              label="Türkçe"
+              type="radio"
+              checked={languageLocale === LOCALES.TURKISH}
+              onChange={(i) =>
+                i.target.checked && setLanguageLocale(i.target.value)
+              }
+            />
+
+            <FormCheck
+              value={LOCALES.ENGLISH}
+              label="English"
+              type="radio"
+              checked={languageLocale === LOCALES.ENGLISH}
+              onChange={(i) =>
+                i.target.checked && setLanguageLocale(i.target.value)
+              }
+            />
+          </Stack>
+        </div>
+        <Home />
+      </>
     </IntlProvider>
   );
 }
