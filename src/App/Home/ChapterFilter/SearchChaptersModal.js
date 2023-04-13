854b03d7b84e9437e98e0c07d55b7d6b0b4f5f5d
mirhalay <mirhalay@tutanota.com>
2023-04-13 15:27:49 +0300

    chapter search improvements

--- src/App/Home/ChapterFilter/SearchChaptersModal.js
+++ src/App/Home/ChapterFilter/SearchChaptersModal.js
@@ -3,55 +3,74 @@ import { FormControl, ListGroup, Modal } from "react-bootstrap";
 import { useIntl } from "react-intl";
 import CheckType from "./CheckType";
 
+function matchChapterName(needle, haystack, lang = "tr") {
+  const hay_ = haystack[lang]
+    .toLocaleUpperCase(lang)
+    .replace("'", "")
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+  const nee_ = needle
+    .toLocaleUpperCase(lang)
+    .replace("'", "")
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+
+  return hay_.match(new RegExp(nee_, "gi"));
+}
+
 export default function SearchChaptersModal({
   onHide,
   chapterList,
   onChapterSelected,
   filterTypeState,
+  currentChapterID,
 }) {
   const { locale, $t } = useIntl();
 
   const [filterText, setFilterText] = useState("");
 
+  const filteredChapters = filterText
+    ? chapterList?.filter(
+        (i) =>
+          matchChapterName(filterText, i, locale.substring(0, 2)) ||
+          i.id.toString().startsWith(filterText)
+      )
+    : chapterList;
+
   return (
     <Modal size="sm" show={chapterList} centered onHide={onHide}>
-      <Modal.Header closeButton>
+      <Modal.Body className="show-grid">
         <FormControl
-          autoFocus
           defaultValue={filterText}
           placeholder={$t({ id: "find_chapter" })}
           onKeyDown={(i) => {
             if (i.key === "Enter") setFilterText(i.target.value?.trim());
           }}
         />
-      </Modal.Header>
-      <Modal.Body className="show-grid">
-        <CheckType className="mb-1" filterTypeState={filterTypeState} />
+        <div className="mb-2 border-bottom">
+          <CheckType className="mb-1" filterTypeState={filterTypeState} />
+        </div>
         {chapterList && (
           <ListGroup style={{ maxHeight: 350, overflowY: "scroll" }}>
-            {chapterList
-              .filter(
-                (i) =>
-                  !filterText ||
-                  i[locale.substring(0, 2)]
-                    .toLowerCase()
-                    .includes(filterText.toLowerCase()) ||
-                  i.id.toString().startsWith(filterText)
-              )
-              .map((i) => (
-                <ListGroup.Item
-                  action="#"
-                  key={i.id}
-                  value={i.id}
-                  onClick={() => onChapterSelected(i.id)}
-                >
-                  <span>
-                    {`${i.id}. ${i[locale.substring(0, 2)]} (${
-                      i.total_verses
-                    } ${$t({ id: "verses" })})`}
-                  </span>
-                </ListGroup.Item>
-              ))}
+            {filteredChapters?.map((i) => (
+              <ListGroup.Item
+                action={i.id !== currentChapterID && "#"}
+                key={i.id}
+                value={i.id}
+                active={i.id === currentChapterID}
+                onClick={
+                  i.id === currentChapterID
+                    ? () => null
+                    : () => onChapterSelected(i.id)
+                }
+              >
+                <span>
+                  {`${i.id}. ${i[locale.substring(0, 2)]} (${
+                    i.total_verses
+                  } ${$t({ id: "verses" })})`}
+                </span>
+              </ListGroup.Item>
+            ))}
           </ListGroup>
         )}
       </Modal.Body>
--- src/App/Home/ChapterFilter/index.js
+++ src/App/Home/ChapterFilter/index.js
@@ -64,6 +64,7 @@ export default function ChapterFilter({
           setSelectedChapterID(chapterID);
         }
       }}
+      currentChapterID={selectedChapterID}
     />
   );
 
